eed293d37672fc8198770bed3d4a807d20e45850
Unknown <99053034@mydavinci.nl>
2020-02-14 14:33:12 +0100

    Welke partijen, belangrijke vragen en nog meer algemenen dingen

--- js/front-end.js
+++ js/front-end.js
@@ -4,18 +4,32 @@ var subjectsTitles = [];
 var subjectsTitlesInQuotes = [];
 var url = location.href;
 var arrowBack = document.getElementById("formArrow");
-var intro = 0;
+var counter = 0;
 var formContainer = document.getElementsByClassName("formContainer")[0];
 var introContainer = document.getElementsByClassName("introContainer")[0];
+var endScreen = document.getElementsByClassName("endScreenContainer")[0];
 var introButton = document.getElementsByClassName("introButton")[0];
-var score = [];
+var endButton = document.getElementById("endScreenArrow");
+var userAwnsers = [];
 var eens = "\"" + "eens" + "\"";
 var geenVanBeide = "\"" + "geen van beide" + "\"";
 var oneens = "\"" + "oneens" + "\"";
+var goBack = false;
+var awnsered = false;
+var awnsersUrl = "";
+var endScreenQuestionsWeights = document.getElementsByClassName("endScreenQuestionsWeights")[0];
+var endScreenPartiesContainer = document.getElementsByClassName("endScreenPartiesContainer")[0];
+var formButton = document.getElementsByClassName("formButton")[0];
+var endScreenQuestions = document.getElementsByClassName("endScreenQuestions")[0];
+var endScreenParties = document.getElementsByClassName("endScreenParties")[0];
+var endScreenPartiesArrow = document.getElementById("endScreenPartiesArrow");
+var questionsImportance = "\"" + "http://localhost/leerjaar%202%20(www)/block%207/challenges/front-end/Challenge-Front-End/index/body.php#questionsImportance" + "\"";
+var seats = ["VVD","PvdA","PVV","SP","CDA","D66","ChristenUnie","GroenLinks","SGP","Partij voor de Dieren","50Plus","VNL","DENK","Forum voor Democratie"];
+var seated = [];
 
-for(b = 0; subjects.length > b; b++){
-	subjectsTitles[b] = subjects[b]["title"];
-	subjectsTitlesInQuotes[b] = "\"" + subjectsTitles[b] + "\""; 
+for(d = 0; subjects.length > d; d++){
+	subjectsTitles[d] = subjects[d]["title"];
+	subjectsTitlesInQuotes[d] = "\"" + subjectsTitles[d] + "\""; 
 }
 
 for(a = 0; subjects.length > a; a++){
@@ -79,14 +93,69 @@ for(a = 0; subjects.length > a; a++){
 		
 	newDiv.appendChild(newCompanies);
 	formContentContainer.appendChild(newDiv);
+
+	var newQuestionWeightContainer = document.createElement("div");
+	newQuestionWeightContainer.className = "questionWeightContainer";
+
+	var newQuestionWeightInput = document.createElement("input");
+	newQuestionWeightInput.className = "questionWeightInput";
+	newQuestionWeightInput.type = "checkbox";
+	newQuestionWeightContainer.appendChild(newQuestionWeightInput);
+
+	var newQuestionWeightSpan = document.createElement("span");
+	newQuestionWeightSpan.innerHTML = subjectsTitles[a];
+	newQuestionWeightContainer.appendChild(newQuestionWeightSpan);
+
+	var newQuestionWeightLine = document.createElement("hr");
+	newQuestionWeightContainer.appendChild(newQuestionWeightLine);
+
+	endScreenQuestionsWeights.appendChild(newQuestionWeightContainer);
+}
+
+for(f = 0; subjects[0]["parties"].length > f; f++){
+	var newPartiesContainer = document.createElement("div");
+	newPartiesContainer.className = "partiesContainer";
+
+	var newPartiestInput = document.createElement("input");
+	newPartiestInput.className = "partiesInput";
+	newPartiestInput.type = "checkbox";
+	newPartiesContainer.appendChild(newPartiestInput);
+
+	var newPartiesSpan = document.createElement("span");
+	newPartiesSpan.innerHTML = subjects[0]["parties"][f]["name"];
+	newPartiesContainer.appendChild(newPartiesSpan);
+
+	var newPartiesLine = document.createElement("hr");
+	newPartiesContainer.appendChild(newPartiesLine);
+
+	endScreenPartiesContainer.appendChild(newPartiesContainer);
 }
 
 introButton.setAttribute("onclick", "page("+subjectsTitlesInQuotes[0]+");");
+endButton.setAttribute("onclick", "back("+subjectsTitlesInQuotes[subjectsTitlesInQuotes.length-1]+");");
+formButton.setAttribute("onclick", "questionWeight();");
+endScreenPartiesArrow.setAttribute("onclick", "back("+questionsImportance+");");
 
-function page(url){
+function page(url = subjectsTitles[subjectsTitles.length-1]){
 	if(url == "http://localhost/leerjaar%202%20(www)/block%207/challenges/front-end/Challenge-Front-End/index/body.php" || url == "http://localhost/leerjaar%202%20(www)/block%207/challenges/front-end/Challenge-Front-End/index/body.php#intro"){
 		formContainer.style.display = "none";
+		endScreen.style.display = "none";
 		introContainer.style.display = "block";
+		endScreenParties.style.display = "none";
+		endScreenQuestions.style.display = "none";
+	}else if(url == "http://localhost/leerjaar%202%20(www)/block%207/challenges/front-end/Challenge-Front-End/index/body.php#questionsImportance"){
+		formContainer.style.display = "none";
+		endScreen.style.display = "block";
+		introContainer.style.display = "none";
+		endScreenParties.style.display = "none";
+		endScreenQuestions.style.display = "block";
+		location.href = originalLink + "#questionsImportance";
+	}else if(url == "http://localhost/leerjaar%202%20(www)/block%207/challenges/front-end/Challenge-Front-End/index/body.php#parties"){
+		formContainer.style.display = "none";
+		endScreen.style.display = "block";
+		introContainer.style.display = "none";
+		endScreenParties.style.display = "block";
+		endScreenQuestions.style.display = "none";
 	}else{
 		url = url.replace('http://localhost/leerjaar%202%20(www)/block%207/challenges/front-end/Challenge-Front-End/index/body.php#','');
 		url = url.replace("%20"," ");
@@ -94,6 +163,10 @@ function page(url){
 			if(subjectsTitles[c] == url){
 				formContainer.style.display = "block";
 				introContainer.style.display = "none";
+				endScreen.style.display = "none";
+				endScreenQuestions.style.display = "none";
+				endScreenParties.style.display = "none";
+
 				arrowBack.setAttribute("onclick", "page("+subjectsTitlesInQuotes[c-1]+");");
 				location.href = originalLink + "#" + url;
 				var formContentSlideContainer = document.getElementsByClassName("formContentSlideContainer");
@@ -111,22 +184,37 @@ function page(url){
 					arrowBack.setAttribute("onclick", "page("+subjectsTitlesInQuotes[c-1]+");");
 				}
 			}else{
-				intro++;
+				counter++;
 			}
 		}
-		if(intro == subjectsTitles.length){
+		if(counter == subjectsTitles.length){
+			awnsered = false;
 			location.href = originalLink + "#intro";
 			url = "http://localhost/leerjaar%202%20(www)/block%207/challenges/front-end/Challenge-Front-End/index/body.php#intro";
 			page(url);
 		}
-		intro = 0;
+		if(subjectsTitles[subjectsTitles.length-1] == url){
+			if(goBack != true){
+				document.getElementsByClassName("formColorBar")[0].style.width = "100%";
+				if(userAwnsers.length == subjectsTitles.length && awnsered == true){
+					location.href = originalLink + "#questionsImportance";
+					url = "http://localhost/leerjaar%202%20(www)/block%207/challenges/front-end/Challenge-Front-End/index/body.php#questionsImportance";
+					page(url);
+				}
+			}
+			goBack = false;
+			awnsered = false;
+		}
+		counter = 0;
 	}
 }
 
 function awnsers(question,awnser,url){
 	var choiceButtons = document.getElementsByClassName("formChoice");
+	awnsersUrl = location.href.replace('http://localhost/leerjaar%202%20(www)/block%207/challenges/front-end/Challenge-Front-End/index/body.php#','');
+	awnsersUrl = awnsersUrl.replace("%20"," ");
 	if(awnser == "eens"){
-		score[question] = "eens";
+		userAwnsers[question] = "eens";
 		var question = question * 3;
 		choiceButton = choiceButtons[question];
 		choiceButton.style.backgroundColor = "#01B4DC";
@@ -137,7 +225,7 @@ function awnsers(question,awnser,url){
 		choiceButtonAlt = choiceButtons[question+2];
 		choiceButtonAlt.style.backgroundColor = "#000";
 	}else if(awnser == "geen van beide"){
-		score[question] = "geen van beide";
+		userAwnsers[question] = "geen van beide";
 		var question = question * 3 + 1;
 		choiceButton = choiceButtons[question];
 		choiceButton.style.backgroundColor = "#01B4DC";
@@ -148,7 +236,7 @@ function awnsers(question,awnser,url){
 		choiceButtonAlt = choiceButtons[question+1];
 		choiceButtonAlt.style.backgroundColor = "#000";
 	}else if(awnser == "oneens"){
-		score[question] = "oneens";
+		userAwnsers[question] = "oneens";
 		var question = question * 3 + 2;
 		choiceButton = choiceButtons[question];
 		choiceButton.style.backgroundColor = "#01B4DC";
@@ -159,9 +247,70 @@ function awnsers(question,awnser,url){
 		choiceButtonAlt = choiceButtons[question-1];
 		choiceButtonAlt.style.backgroundColor = "#000";
 	}else{
-		score[question] = "";
+		userAwnsers[question] = "";
+	}
+	if(subjectsTitles[subjectsTitles.length-1] == awnsersUrl){
+		awnsered = true;
 	}
 	page(url);
+}
+
+function back(url,buttonCommand = true){
+	goBack = buttonCommand;
+	page(url);
+}
+
+function questionWeight(){
+	var getInputs = [];
+	var inputValues = [];
+	for(g = 0; document.getElementsByClassName("questionWeightInput").length > g; g++){
+		getInputs[g] = document.getElementsByClassName("questionWeightInput")[g];
+	}
+	for(e = 0; subjectsTitles.length > e; e++){
+		inputValues[e] = getInputs[e].checked;
+	}
+	var newUrl = location.href = originalLink + "#parties";
+	parties(newUrl);
+}
+
+function parties(url){
+	page(url);
+}
+
+function select(type){
+	var getInputs = [];
+	var inputValues = [];
+	for(g = 0; document.getElementsByClassName("partiesInput").length > g; g++){
+		getInputs[g] = document.getElementsByClassName("partiesInput")[g];
+	}
+	if(type == "all"){
+		for(e = 0; subjectsTitles.length > e; e++){
+			if(getInputs[e]){
+				getInputs[e].checked = true;
+				inputValues[e] = getInputs[e].checked;
+			}
+		}
+	}else if(type == "none"){
+		for(e = 0; subjectsTitles.length > e; e++){
+			if(getInputs[e]){
+				getInputs[e].checked = false;
+				inputValues[e] = getInputs[e].checked;
+			}
+		}
+	}else if(type == "allSeats"){
+		for(e = 0; subjectsTitles.length > e; e++){
+			for(h = 0; subjects[0]["parties"].length > h; h++){
+				seated[h] = seats.includes(subjects[0]["parties"][h]["name"]);
+			}
+			if(getInputs[e] && seated[e] == true){
+				getInputs[e].checked = true;
+				inputValues[e] = getInputs[e].checked;
+			}else{
+				getInputs[e].checked = false;
+				inputValues[e] = getInputs[e].checked;
+			}
+		}
+	}
 }
 
 page(url);
--- pages/challenges-front-end.php
+++ pages/challenges-front-end.php
@@ -15,4 +15,70 @@
 			<div class="formInnerContentContainer"></div>
 		</div>
 	</div>
+	<div class="endScreenContainer">
+		<div class="endScreenQuestions">
+			<div class="endScreenColorBar"></div>
+			<div class="formArrowContainer">
+				<i id="endScreenArrow" class="fas fa-arrow-left"></i>
+			</div>
+			<div class="formContentContainer">
+				<div class="formInnerContentContainer">
+					<div class="endScreenQuestionsHeader">
+						<h2>
+							Zijn er onderwerpen die u extra belangrijk vindt?
+						</h2>
+						<p>
+							Aangevinkte stellingen tellen extra mee bij het berekenen van het resulaat.
+						</p>
+						<button class="formButton">
+							Ga verder
+						</button>
+					</div>
+					<div class="endScreenQuestionsWeights">
+						<h3>
+							Extra belangrijke onderwerpen
+						</h3>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="endScreenParties">
+			<div class="endScreenColorBar"></div>
+				<div class="formArrowContainer">
+					<i id="endScreenPartiesArrow" class="fas fa-arrow-left"></i>
+				</div>
+				<div class="formContentContainer">
+					<div class="formInnerContentContainer">
+						<div class="endScreenPartiesHeader">
+							<h2>
+								Welke partijen wilt u meenemen in het resultaat?
+							</h2>
+							<p>
+								U kunt kiezen voor <strong>zittende partijen</strong>, die nu in de Tweede Kamer vertegenwoordigd zijn. Daarbij nemen we ook de partijen mee die in de peilingen op minimaal één zetel staan. U kunt <strong>alle partijen</strong> meenemen en u kunt een <strong>eigen selectie</strong> maken van tenminste drie partijen.
+							</p>
+							<button class="formButton">
+								Naar resultaat
+							</button>
+						</div>
+						<div class="endScreenPartiesContainer">
+							<div class="PartiesHeader">
+								<h3>
+									Kies de partijen die u mee wilt nemen in het resultaat
+								</h3>
+								<button onclick="select('allSeats');">
+									Selecteer zittende partijen
+								</button>
+								<button onclick="select('all');">
+									Selecteet alle partijen
+								</button>
+								<button onclick="select('none');">
+									Verwijder selectie
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
